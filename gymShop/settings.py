a5a42267f1a7fc77f2acfca4d8339797416a7845
Kartik <iamkaranmishra551@gmail.com>
2023-12-09 13:24:19 +0530

    Add app to our project.

--- gymShop/settings.py
+++ gymShop/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'fitness.apps.FitnessConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
