fdb3fdc22a8ae22cab4c26108fc89d6bdbbbbed2
Kartik <iamkaranmishra551@gmail.com>
2023-12-25 06:52:17 +0530

    Create a model to register a user.

--- fitness/models.py
+++ fitness/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Register(models.Model):
+    name = models.CharField(max_length=100)
+    contact = models.CharField(max_length=100)
+    email = models.EmailField(max_length=250)
+    address = models.CharField(max_length=500)
+    password = models.CharField(max_length=200, default=" ")
+
+    def __str__(self):
+        return self.name
